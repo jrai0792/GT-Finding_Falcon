cc5e69b4a4b955e1a5f462125bbb087244f32154
Jyoti <jyotikiran12@gmail.com>
2021-05-31 17:34:03 +0530

    Webpack setup added

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true,
+        title: "Finding Falcon",
+        myPageHeader: 'Hello World',
+        filename: './index.html' //relative to root of the application
+    })
+  ]
+  
+};
